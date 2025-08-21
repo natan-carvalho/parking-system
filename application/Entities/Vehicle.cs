0dad40f15450a89e5bd092da66fc6fed4f6a0c86
Natanael de Carvalho Santos <natan.carvalho123@hotmail.com>
2025-08-21 17:35:08 +0000

    :sparkles: feat: finish the project

--- application/Entities/Vehicle.cs
+++ application/Entities/Vehicle.cs
@@ -16,4 +16,9 @@ public class Vehicle
       _licensePlate = value;
     }
   }
+
+  public override string ToString()
+  {
+    return $"Placa: {_licensePlate}";
+  }
 }
--- application/Program.cs
+++ application/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using application.Services;
+
+bool startSystem = true;
+ParkingService parkingService = new();
+
+while (startSystem)
+{
+  // Console.Clear();
+  Console.WriteLine("\nBem-vindo ao sistema de estacionamento.");
+  Console.WriteLine("\nEscolha uma opção:");
+  Console.WriteLine("1 - Cadastrar um veículo.");
+  Console.WriteLine("2 - Remover um veículo.");
+  Console.WriteLine("3 - Listar todos os veículos.");
+  Console.WriteLine("4 - Sair.");
+
+  var value = Console.ReadLine();
+
+  switch (value)
+  {
+    case "1":
+      Console.WriteLine("Informe a placa do veículo:");
+      var licensePlate = Console.ReadLine();
+      parkingService.RegisterVehicle(licensePlate);
+      break;
+    case "2":
+      Console.WriteLine("Informe a placa do veículo:");
+      var licensePlateRemove = Console.ReadLine();
+      parkingService.RemoveVehicle(licensePlateRemove);
+      break;
+    case "3":
+      var vehicles = parkingService.GetParkedVehicles();
+      Console.WriteLine("Placa:");
+      int index = 0;
+      vehicles.ForEach(vehicle =>
+      {
+        Console.WriteLine($"{index} - {vehicle.LicensePlate}");
+        index++;
+      });
+      break;
+    case "4":
+      startSystem = false;
+      break;
+    default:
+      Console.WriteLine("Opção invalida, favor escolha uma opção valida.");
+      break;
+  }
+}
