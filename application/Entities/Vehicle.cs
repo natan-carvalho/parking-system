32d4c8ccbb679959190eebfd37984cc5d59190bd
Natanael de Carvalho Santos <natan.carvalho123@hotmail.com>
2025-08-22 13:30:25 +0000

    :ok_hand: update: added more attribute in Vehicle entity

--- application/Entities/Vehicle.cs
+++ application/Entities/Vehicle.cs
@@ -3,17 +3,39 @@ namespace application.Entities;
 public class Vehicle
 {
   private string _licensePlate;
+  private float _cost;
+  private DateTime _entryTime = DateTime.Now;
 
   public string LicensePlate
   {
     get => _licensePlate;
     set
     {
-      if (string.IsNullOrWhiteSpace(value))
-      {
-        throw new ArgumentNullException(nameof(LicensePlate), "Favor informe um valor valido.");
-      }
+      Validation(value, "Favor informe um valor valido.");
       _licensePlate = value;
+    }
+  }
+
+  public float Cost
+  {
+    get => _cost;
+    set
+    {
+      Validation(value, "Favor informe um valor valido.");
+      _cost = value;
+    }
+  }
+
+  public DateTime EntryTime
+  {
+    get => _entryTime;
+  }
+
+  private static void Validation<T>(T value, string message)
+  {
+    if (string.IsNullOrWhiteSpace(value?.ToString()) || float.Parse(value?.ToString()) < 0)
+    {
+      throw new ArgumentNullException(nameof(value), message);
     }
   }
 }
